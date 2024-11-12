48cb582330159b5cb281debb2dfe3b0e27f4b5d5
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-12 14:51:43 +0100

    added constructor and tostring override

--- classes/Model.cs
+++ classes/Model.cs
@@ -1,5 +1,16 @@
 public class Model
 {
-    public string? name { get; set; }
-    public int strength { get; set; }
+    public string? Name { get; set; }
+    public int Strength { get; set; }
+
+    public Model(string name, int strength)
+    {
+        Name = name;
+        Strength = strength;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name} Deadlift: {Strength}kg.";
+    }
 }
