2a9175fb0c6de056b5a9287c119065128794a0ad
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-13 11:01:17 +0100

    removed extra code + added comments

--- classes/View.cs
+++ classes/View.cs
@@ -1,23 +1,14 @@
 public class View
 {
-
+    //Displays the current state of the provided Model object to the user(the Model object is provided through writing (Model model)).
     public void Display(Model model)
     {
         Console.WriteLine(model.ToString());
     }
 
+    //Allows displaying any custom message to the user, independently of Display(Model model)
     public void DisplayMessage(string message)
     {
         Console.WriteLine($"{message}");
     }
-
-    //public string nameMethod()
-    //{
-    //    return controller.AddName();
-    //}
-    //
-    //public string strengthMethod()
-    //{
-    //    return controller.strength();
-    //}
 }
