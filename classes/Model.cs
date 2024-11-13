137f08b91bffe91d38d32036741f666997449c10
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-13 09:30:53 +0100

    added comments to the model class

--- classes/Model.cs
+++ classes/Model.cs
@@ -1,14 +1,17 @@
 public class Model
 {
+    //created two properties, one for Name and one for Strength
     public string? Name { get; set; }
     public int Strength { get; set; }
 
+    //created a constructor to initialize the Name and Strength properties, allowing them to be set when a Model instance is created
     public Model(string name, int strength)
     {
         Name = name;
         Strength = strength;
     }
 
+    //overridden ToString() to provide a formatted message displaying the name and strength, which the view class can use to display to the user
     public override string ToString()
     {
         return $"Name: {Name} Deadlift: {Strength}kg.";
