16ededbf0e450041680a4ddf4b47122d2d1150ac
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-13 11:45:14 +0100

    added comments to the program file

--- Program.cs
+++ Program.cs
@@ -4,18 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Create new instances of the Model, View and Controller classes
         Model model = new Model("Wendy Chan", 168);
         View view = new View();
         Controller controller = new Controller(model, view);
 
+        //Test displaying the initial Model data
         controller.DisplayModel();
 
+        //Test the AddName and deadLift methods
         controller.AddName();
 
         controller.deadLift();
 
+        //Display the updated Model to check if the user input was saved
         controller.DisplayModel();
 
+        //Keep the program running
         Console.ReadLine();
 
 
